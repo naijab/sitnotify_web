fe5c8c5cbe82981d0c600c8632a84fc2d64d3e6c
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-05 16:02:51 +0700

    Edit NewsSchema

--- api/models/NewsModel.js
+++ api/models/NewsModel.js
@@ -1,1 +1,56 @@
-2
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var NewsSchema = new Schema({
+    title: {
+        type: String,
+        Required: 'enter title of news'
+    },
+    image: {
+        type: String,
+    },
+    description: {
+        type: String,
+        Required: 'enter description of news'
+    },
+    category: {
+        type: String,
+        Required: 'enter category of news'
+    },
+    level: {
+        type: [{
+            type: String,
+            enum: ['Bachelor Degrees', 'Master Degrees', 'Doctor Degrees']
+        }],
+        default: ['Bachelor']
+    },
+    program: {
+        type: [{
+            type: String,
+            enum: ['IT', 'CS', 'BIS', 'SE', 'Bio', 'IT (Weekend)', 'IT (Night)']
+        }],
+        default: ['IT']
+    },
+    link: {
+        type: String,
+    },
+    attached_file: {
+        type: String,
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+    edited_date: {
+        type: Date,
+        default: Date.now
+    },
+    is_important: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('News', NewsSchema);
