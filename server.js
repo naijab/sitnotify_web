1a4d09159070e3cd4be1417acb8350d9884f49cc
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-06 19:06:34 +0700

    Add variable and import node

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 5000;
+  mongoose = require('mongoose'),
+  News = require('./api/models/News'),
+  bodyParser = require('body-parser');
+  config = require('config')
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.connectionString); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/Routes');
+routes(app);
 
 app.listen(port);
 
