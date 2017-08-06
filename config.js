1f296e0183811a38705d562038741f98ed7e987c
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-06 21:32:03 +0700

    Edit Config file

--- config.js
+++
@@ -1,1 +0,0 @@
-// export const connectionString = 'mongodb://sigmadrivedb:KaHfiVrkqKBjdbnvRY97TncDuKRocBgrUPcGbd4SApZmJYNXVkoSwFrbj6lwemrM99wTf45dgsV9zLd8bh4phw==@sigmadrivedb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var express = require('express'),
   port = process.env.PORT || 5000;
   mongoose = require('mongoose'),
   News = require('./api/models/newsModel'),
-  bodyParser = require('body-parser');
-  // config = require('./config')
+  bodyParser = require('body-parser'),
+  config = require('config.json');
+
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://sigmadrivedb:KaHfiVrkqKBjdbnvRY97TncDuKRocBgrUPcGbd4SApZmJYNXVkoSwFrbj6lwemrM99wTf45dgsV9zLd8bh4phw==@sigmadrivedb.documents.azure.com:10255/?ssl=true'); 
+mongoose.connect(config.connectionString); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
