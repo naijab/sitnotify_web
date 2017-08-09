52c9ed0f86cb202fc7ff88feb02e9136b2428757
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-09 23:16:56 +0700

    Add news route restful

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var newsList = require('../controllers/newsController');
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/news')
+  .get(newsList.list_all_news)
+  .post(newsList.create_a_news);
 
-router.get('/hello', function(req, res, next) {
-  res.send('hello');
-});
+router.route('/news/:newsId')
+  .get(newsList.read_a_news)
+  .put(newsList.update_a_news)
+  .delete(newsList.delete_a_news);
 
 module.exports = router;
