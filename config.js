dcb890161fa5d0de5c9a12e748c7354a22d5f4e3
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-09 17:40:57 +0700

    Remove All file

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'connectionString': 'mongodb://sigmadrivedb:KaHfiVrkqKBjdbnvRY97TncDuKRocBgrUPcGbd4SApZmJYNXVkoSwFrbj6lwemrM99wTf45dgsV9zLd8bh4phw==@sigmadrivedb.documents.azure.com:10255/?ssl=true'
-}
