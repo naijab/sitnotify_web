78c955293a31f3819f23f67e5acede4a984577a6
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-09 23:32:04 +0700

    Add secret

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'connectionString': 'mongodb://sigmadrivedb:KaHfiVrkqKBjdbnvRY97TncDuKRocBgrUPcGbd4SApZmJYNXVkoSwFrbj6lwemrM99wTf45dgsV9zLd8bh4phw==@sigmadrivedb.documents.azure.com:10255/?ssl=true'
+    'connectionString': 'mongodb://sigmadrivedb:KaHfiVrkqKBjdbnvRY97TncDuKRocBgrUPcGbd4SApZmJYNXVkoSwFrbj6lwemrM99wTf45dgsV9zLd8bh4phw==@sigmadrivedb.documents.azure.com:10255/?ssl=true',
+    'secreteKey': 'Dy0XS0KSL2kTCV9wTIrvOAC4S12NFeugR'
 }
