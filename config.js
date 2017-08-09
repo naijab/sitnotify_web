5251d0745e12e9439d3ce0c35bfeaa493eba362a
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-09 23:32:28 +0700

    Add secret key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'connectionString': 'mongodb://sigmadrivedb:KaHfiVrkqKBjdbnvRY97TncDuKRocBgrUPcGbd4SApZmJYNXVkoSwFrbj6lwemrM99wTf45dgsV9zLd8bh4phw==@sigmadrivedb.documents.azure.com:10255/?ssl=true',
-    'secreteKey': 'Dy0XS0KSL2kTCV9wTIrvOAC4S12NFeugR'
+    'secretKey': 'Dy0XS0KSL2kTCV9wTIrvOAC4S12NFeugR'
 }
